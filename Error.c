6dab53f85097c1a174d862219998eb6ec189477d
Nikita <nikitastrel4@gmail.com>
2022-04-26 14:46:45 +0400

    Добавляю домашнее задание а так же вы меня попросили скинуть Алгоритм с оишбкой на языке С его вы сможете найти в файле error.c

---
+++ Error.c
@@ -0,0 +1,41 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+void PrimeInt();
+
+int main ()
+{
+	PrimeInt();
+}
+
+void PrimeInt() {
+	
+	int x = 2;
+	int i = 0;
+	printf("Hi pleas enter number from 2 till 1000\n");
+	int a = 4;
+	scanf("%d", &a);
+
+	while (a >= x)
+	{
+		if (a % x == 0)
+		{
+			i++;
+		}
+		else 
+		{
+			x++;
+		}
+	}
+
+	if (i == 1)
+	{
+		printf("is prime number");
+	}
+	else
+	{
+		printf("is not prime number");
+	}
+	
+}
