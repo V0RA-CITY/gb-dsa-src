b126370a651b6c79b2ffd5489f547d24fc4e1d8d
Nikita <nikitastrel4@gmail.com>
2022-04-29 23:21:04 +0400

    add home work lesson 4

--- ConsoleApplication6.c
+++ ConsoleApplication6.c
@@ -1,42 +1,113 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
+﻿#include <stdio.h>
 
-#include <stdio.h>
+void transformation(int x) {
+	if (x > 0) {
+		printf("%i", x % 2);
+		transformation(x / 2);
+	}
+		
+}
 
-void PrimeInt();
+void exponentiation(int a, int b) {
+	int sum = a;
+	if (a > 1 && b >= 1)
+	{
+		for (int i = 1; i < b; i++)
+		{
+			sum = a * sum;
+		}
+		printf("\n%i", sum);
+	}
+	else if (a == 0)
+	{
+		printf("\n%i", 0);
+	}
+	else if (b == 0)
+	{
+		printf("\n%i", 1);
+	}else 
+		printf("\n%i", 1);
+	
+	
+}
+	
+int RecursiveExponentiation(int a, int b) {
+	
+	if (a == 0)
+		return 0;
+	if (b == 0)
+		return 1;
+	if (a == 1)
+		return 1;
+	if (a > 1 && b >= 1)
+	{
+		int sum = 0;
+		sum = a * RecursiveExponentiation(a, b - 1);
+		return sum;
+	}
+	
+
+	
+}
+
+int RecursiveExponentiationAlter(int a, int b) {
+
+	if (a == 0)
+		return 0;
+	if (b == 0)
+		return 1;
+	if (a == 1)
+		return 1;
+	if (a > 1 && b >= 1  && b % 2 == 1)
+	{
+		int sum = 0;
+		sum = a * RecursiveExponentiation(a, b - 1);
+		return sum;
+	}
+
+	if (a > 1 && b >= 1 && b % 2 == 0)
+	{
+		int sum = 0;
+		sum = a * RecursiveExponentiation(a, b / 2);
+		return sum;
+	}
+
+
+}
+
+int routes(int x, int y) {
+	if (x == 0 && y == 0)
+		return 0;
+	else if (x == 0 ^ y == 0)
+		return 1;
+	if (x == 3 && y == 2)
+		return routes(x - 1, y -1);
+
+
+	return routes(x, y - 1) + routes(x - 1, y);
+}
+
 
 int main ()
 {
-	PrimeInt();
-}
+	printf("%s", "Read Binary number from right to left\n");
+	transformation(12);
+	exponentiation(5, 5);
+	printf("\n%d", RecursiveExponentiation(5, 5));
+	printf("\n%d", RecursiveExponentiationAlter(5, 5));
 
-void PrimeInt() {
-	
-	int x = 2;
-	int i = 0;
-	printf("Hi pleas enter number from 2 till 1000\n");
-	int a = 4;
-	scanf("%d", &a);
+	const int sizeX = 5;
+	const int sizeY = 5;
 
-	while (a >= x)
+	printf("\n");
+	for (int y = 0; y < sizeY; y++)
 	{
-		if (a % x == 0)
+		for (int x = 0; x < sizeX; x++)
 		{
-			i++;
-			x++;
+			printf("%5d", routes(x, y));
 		}
-		else 
-		{
-			x++;
-		}
-	}
-
-	if (i == 1)
-	{
-		printf("is prime number");
-	}
-	else
-	{
-		printf("is not prime number");
+		printf("\n");
 	}
 	
+		
 }
