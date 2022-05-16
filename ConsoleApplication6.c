804eac43981abd607da044d92f71bd6dfe3084a5
Nikita <nikitastrel4@gmail.com>
2022-05-16 17:58:03 +0400

    add home work lesson 6

--- ConsoleApplication6.c
+++ ConsoleApplication6.c
@@ -1,113 +1,103 @@
-﻿#include <stdio.h>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
 
-void transformation(int x) {
-	if (x > 0) {
-		printf("%i", x % 2);
-		transformation(x / 2);
+
+void BubbleSort(int Arr[3][3])
+{
+	for (int i = 0; i < 9; i++)
+	{
+		for (int i = 0; i < 3; i++)
+		{
+
+
+			for (int j = 0; j < 2; j++)
+			{
+				if (Arr[i][j] > Arr[i][j + 1])
+				{
+					int t = Arr[i][j];
+					Arr[i][j] = Arr[i][j + 1];
+					Arr[i][j + 1] = t;
+				}
+			}
+
+			if (i < 2)
+			{
+				if (Arr[i][2] > Arr[i + 1][0])
+				{
+					int t = Arr[i][2];
+					Arr[i][2] = Arr[i + 1][0];
+					Arr[i + 1][0] = t;
+				}
+			}
+
+		}
 	}
 		
-}
-
-void exponentiation(int a, int b) {
-	int sum = a;
-	if (a > 1 && b >= 1)
-	{
-		for (int i = 1; i < b; i++)
-		{
-			sum = a * sum;
-		}
-		printf("\n%i", sum);
-	}
-	else if (a == 0)
-	{
-		printf("\n%i", 0);
-	}
-	else if (b == 0)
-	{
-		printf("\n%i", 1);
-	}else 
-		printf("\n%i", 1);
+	
+		
+		
 	
 	
 }
-	
-int RecursiveExponentiation(int a, int b) {
-	
-	if (a == 0)
-		return 0;
-	if (b == 0)
-		return 1;
-	if (a == 1)
-		return 1;
-	if (a > 1 && b >= 1)
-	{
-		int sum = 0;
-		sum = a * RecursiveExponentiation(a, b - 1);
-		return sum;
-	}
-	
 
-	
-}
-
-int RecursiveExponentiationAlter(int a, int b) {
-
-	if (a == 0)
-		return 0;
-	if (b == 0)
-		return 1;
-	if (a == 1)
-		return 1;
-	if (a > 1 && b >= 1  && b % 2 == 1)
-	{
-		int sum = 0;
-		sum = a * RecursiveExponentiation(a, b - 1);
-		return sum;
-	}
-
-	if (a > 1 && b >= 1 && b % 2 == 0)
-	{
-		int sum = 0;
-		sum = a * RecursiveExponentiation(a, b / 2);
-		return sum;
-	}
-
-
-}
-
-int routes(int x, int y) {
-	if (x == 0 && y == 0)
-		return 0;
-	else if (x == 0 ^ y == 0)
-		return 1;
-	if (x == 3 && y == 2)
-		return routes(x - 1, y -1);
-
-
-	return routes(x, y - 1) + routes(x - 1, y);
-}
-
-
-int main ()
+void print(int Arr[][3])
 {
-	printf("%s", "Read Binary number from right to left\n");
-	transformation(12);
-	exponentiation(5, 5);
-	printf("\n%d", RecursiveExponentiation(5, 5));
-	printf("\n%d", RecursiveExponentiationAlter(5, 5));
-
-	const int sizeX = 5;
-	const int sizeY = 5;
-
-	printf("\n");
-	for (int y = 0; y < sizeY; y++)
+	for (int i = 0; i < 3; i++)
 	{
-		for (int x = 0; x < sizeX; x++)
+		for (int j = 0; j < 3; j++)
 		{
-			printf("%5d", routes(x, y));
+			printf("%i", Arr[i][j]);
 		}
 		printf("\n");
 	}
+}
+
+void TPK()
+{
+	int Arr[11] ;
+	printf("%s", "Введите 11 чисел: ");
 	
-		
+	for (int i = 0; i < 11; i++)
+	{
+		int t;
+		scanf("%i", &t);
+		Arr[i] = t;
+	}
+
+	int left = 0;
+	int right = 10;
+
+	while (left < 6)
+	{
+		int temp = Arr[left];
+		Arr[left] = Arr[right];
+		Arr[right] = temp;
+		left++;
+		right--;
+	}
+
+	for (int i = 0; i < 11; i++)
+	{
+		Arr[i] = sqrt(fabs(Arr[i])) + 5 * pow(Arr[i], 3);
+
+		if (Arr[i] > 400)
+		{
+			printf("%s%i%s\n", "Результат элемента ", i ," превысил 400");
+		}
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	int Arr[3][3] = {{1,9,2},
+					{5, 7, 6},
+					{4, 3, 8}};
+
+	BubbleSort(Arr);
+	print(Arr);
+	TPK();
 }
