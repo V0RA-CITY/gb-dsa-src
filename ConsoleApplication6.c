8581c349cdaac380f2003fc3af3854d9e4f70c0c
Nikita <nikitastrel4@gmail.com>
2022-05-21 15:53:17 +0400

    add half of homeWork

--- ConsoleApplication6.c
+++ ConsoleApplication6.c
@@ -1,103 +1,107 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
+#define SIZE 30 // Не знаю почему но работает только черезе define иначе не даёт использовать константу SIZE при объявлении массива
 #include <stdio.h>
 #include <locale.h>
 #include <math.h>
+#include <malloc.h>
 
-
-void BubbleSort(int Arr[3][3])
+void swap(int* a, int* b)
 {
-	for (int i = 0; i < 9; i++)
+	int t = *a;
+	*a = *b;
+	*b = t;
+}
+
+void initArr(int* Arr, int length)
+{
+	for (int i = 0; i < length; i++)
 	{
-		for (int i = 0; i < 3; i++)
+		Arr[i] = rand() % 100;
+	}
+}
+
+void print(int* Arr, int length)
+{
+	for (int i = 0; i < length; i++)
+	{
+			printf(" %i", Arr[i]);
+	}
+}
+
+void Median(int* Arr)
+
+{
+	for (int i = 0; i < 3 - 1; i++)
+	{
+		for (int j = 0; j < 3-1; j++)
 		{
-
-
-			for (int j = 0; j < 2; j++)
+			if (Arr[j] > Arr[j+1])
 			{
-				if (Arr[i][j] > Arr[i][j + 1])
-				{
-					int t = Arr[i][j];
-					Arr[i][j] = Arr[i][j + 1];
-					Arr[i][j + 1] = t;
-				}
+				swap(&Arr[j], &Arr[j+1]);
+			}
+		}
+	}
+}
+
+void qSort(int* Arr, int first, int last)
+{
+	
+	int i = first;
+	int j = last;
+	
+
+	if (last <= 10)
+	{
+		int temp, pos;
+
+		for (int i = 1; i < last; i++)
+		{
+			temp = Arr[i];
+			pos = i - 1;
+
+			while (pos >= 0 && Arr[pos] > temp)
+			{
+				Arr[pos + 1] = Arr[pos];
+				pos--;
+			}
+			Arr[pos + 1] = temp;
+		}
+	}
+	else
+	{
+		int ArrT[3] = { Arr[first], Arr[last], Arr[(first + last) / 2] };
+		Median(ArrT);
+		int x = ArrT[1];
+		do
+		{
+			while (Arr[i] < x) i++;
+			while (Arr[j] > x) j--;
+
+			if (i <= j)
+			{
+				swap(&Arr[i], &Arr[j]);
+				i++;
+				j--;
 			}
 
-			if (i < 2)
-			{
-				if (Arr[i][2] > Arr[i + 1][0])
-				{
-					int t = Arr[i][2];
-					Arr[i][2] = Arr[i + 1][0];
-					Arr[i + 1][0] = t;
-				}
-			}
 
-		}
+		} while (i <= j);
+		if (i < last) qSort(Arr, i, last);
+		if (j > first) qSort(Arr, first, j);
 	}
-		
+
 	
+
 		
-		
-	
-	
 }
 
-void print(int Arr[][3])
-{
-	for (int i = 0; i < 3; i++)
-	{
-		for (int j = 0; j < 3; j++)
-		{
-			printf("%i", Arr[i][j]);
-		}
-		printf("\n");
-	}
-}
-
-void TPK()
-{
-	int Arr[11] ;
-	printf("%s", "Введите 11 чисел: ");
-	
-	for (int i = 0; i < 11; i++)
-	{
-		int t;
-		scanf("%i", &t);
-		Arr[i] = t;
-	}
-
-	int left = 0;
-	int right = 10;
-
-	while (left < 6)
-	{
-		int temp = Arr[left];
-		Arr[left] = Arr[right];
-		Arr[right] = temp;
-		left++;
-		right--;
-	}
-
-	for (int i = 0; i < 11; i++)
-	{
-		Arr[i] = sqrt(fabs(Arr[i])) + 5 * pow(Arr[i], 3);
-
-		if (Arr[i] > 400)
-		{
-			printf("%s%i%s\n", "Результат элемента ", i ," превысил 400");
-		}
-	}
-}
 
 int main()
 {
-	setlocale(LC_ALL, "Rus");
-
-	int Arr[3][3] = {{1,9,2},
-					{5, 7, 6},
-					{4, 3, 8}};
-
-	BubbleSort(Arr);
-	print(Arr);
-	TPK();
+	int Arr[SIZE];
+	initArr(Arr, SIZE);
+	print(Arr, SIZE);
+	qSort(Arr, 0, SIZE - 1);
+	printf("\n");
+	print(Arr, 30);
 }
