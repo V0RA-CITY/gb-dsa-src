af0065206044f424891a2f5f7490029ac78325de
Nikita <nikitastrel4@gmail.com>
2022-06-02 17:54:28 +0400

    add home work

--- ConsoleApplication6.c
+++ ConsoleApplication6.c
@@ -1,92 +1,214 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
-#define SIZE 10
-#define SZ 1000
-#define T char
 #define true 1==1
 #define false 1!=1
 #include <stdio.h>
 #include <locale.h>
 #include <math.h>
 #include <malloc.h>
+#include <stdlib.h>
 
 typedef int boolean;
-
 typedef struct Node
 {
-	int dat;
-	struct Node *next;
+	int key;
+	struct Node* left;
+	struct Node* right;
+} TreeNode;
 
-} Node;
+int counter1 = 0;
+int counter2 = 0;
 
-typedef struct
+TreeNode* treeInsert(TreeNode* t, int data)
 {
-	Node *head;
-	int size;
+	TreeNode* newNode;
+	newNode = (TreeNode*)malloc(sizeof(TreeNode));
+	newNode->key = data;
+	newNode->left = NULL;
+	newNode->right = NULL;
+	TreeNode* current = t;
+	TreeNode* parent = t;
 
-}list;
-
-
-
-int cursor = -1;
-T Stack[SZ];
-
-boolean push(T data) {
-	if (cursor < SZ)
+	if (t == NULL)
 	{
-		Stack[++cursor] = data;
-		return true;
+		t = newNode;
 	}
 	else
 	{
-		printf("%s", "Stack overflow");
-		return false;
-	}
-}
-
-T pop() {
-	if (cursor != -1)
-	{
-		return Stack[cursor--];
-	}
-	else {
-		printf("%s", "Stack is empty");
-		return -1;
-	}
-}
-
-boolean sequence()
-{
-	int prev;
-	char str[SZ];
-	printf("Write sequence :");
-	scanf("%s", &str);
-
-	for (int i = 0; i < SZ; i++)
-	{
-		if (str[i] == '(' || str[i] == '[' || str[i] == '{')
+		while (current->key != data)
 		{
-			push(str[i]);
-		}
-
-		if (str[i] == ')' || str[i] == ']' || str[i] == '}')
-		{
-			if (cursor == -1)
+			parent = current;
+			if (current->key > data)
 			{
-				return false;
-			}
-
-			for (int j = 0; j <= cursor; j++)
-			{
-				if ((int)Stack[j] == (int)(str[i] - 1) || (int)Stack[j] == (int)(str[i] - 2))
+				current = current->left;
+				if (current == NULL)
 				{
-					pop();
-					continue;
+					parent->left = newNode;
+					return t;
+				}
+			}
+			else
+			{
+				current = current->right;
+				if (current == NULL)
+				{
+					parent->right = newNode;
+					return t;
 				}
 			}
 		}
 	}
+	return t;
+}
 
-	if (cursor == -1)
+void printTree(TreeNode* root)
+{
+	if (root)
+	{
+		printf("%i", root->key);
+		if (root->left || root->right)
+		{
+			printf("(");
+			if (root->left)
+			{
+				printTree(root->left);
+			}
+			else
+			{
+				printf("NULL");
+			}
+			printf(",");
+			if (root->right)
+			{
+				printTree(root->right);
+			}
+			else
+			{
+				printf("NULL");
+			}
+			printf(")");
+		}
+	}
+}
+
+TreeNode* getSuccessor(TreeNode* node)
+{
+	TreeNode* current = node->right;
+	TreeNode* parent = node;
+	TreeNode* s = node;
+
+	while (current != NULL)
+	{
+		parent = s;
+		s = current;
+		current = current->left;
+	}
+	if (s != node->right)
+	{
+		parent->left = s->right;
+		s->right = node->right;
+	}
+	return s;
+}
+
+boolean delete(TreeNode* root, int key)
+{
+	TreeNode* current = root;
+	TreeNode* parent = root;
+	boolean isLeftChild = true;
+
+	while (current->key != key)
+	{
+		parent = current;
+		if (key < current->key)
+		{
+			current = current->left;
+			isLeftChild = true;
+		}
+		else
+		{
+			current = current->right;
+			isLeftChild = false;
+		}
+		if (current = NULL)
+		{
+			return false;
+		}
+	}
+
+	if (current->left == NULL && current->right == NULL)
+	{
+		if (current == root)
+		{
+			root = NULL;
+		}
+		else if (isLeftChild)
+		{
+			parent->left = NULL;
+		}
+		else
+		{
+			parent->right = NULL;
+		}
+	}
+	else if (current->right == NULL)
+	{
+		if (isLeftChild)
+		{
+			parent->left = current->left;
+		}
+		else
+			parent->right = current->left;
+	}
+	else if (current->left == NULL)
+	{
+		if (isLeftChild)
+		{
+			parent->left = current->right;
+		}
+		else
+			parent->right = current->right;
+	}
+	else
+	{
+		TreeNode* successor = getSuccessor(current);
+		if (current == root)
+		{
+			root == successor;
+		}
+		else if (isLeftChild)
+		{
+			parent->left = successor;
+		}
+		else
+			parent->right = successor;
+		successor->left = current->left;
+	}
+	return true;
+}
+
+boolean balanceDetect(TreeNode* root)
+{
+	if (root == NULL)
+	{
+		counter1 /= 2;
+		counter2 /= 2;
+	}
+
+	if (root)
+	{
+		balanceDetect(root->left);
+		balanceDetect(root->right);
+		if (root->left)
+		{
+			counter1++;
+		}
+		if (root->right)
+		{
+			counter2++;
+		}
+	}
+
+	if (counter1 == counter2 || counter1 == counter2 + 1 || counter1 == counter2 - 1)
 	{
 		return true;
 	}
@@ -94,159 +216,71 @@ boolean sequence()
 	{
 		return false;
 	}
-}
-
-void initList (list* lst)
-{
-	lst->head = NULL;
-	lst->size = 0;
-}
-
-void ListPush(list* lst, int value)
-{
-	Node* new = (Node*)malloc(sizeof(Node));
-	new->dat = value;
-	new->next = NULL;
-
-	Node* current = lst->head;
-
-	if (current == NULL)
-	{
-		lst->head = new;
-		lst->size++;
-		return;
-	}
-	else
-	{
-		while (current->next != NULL)
-		{
-			current = current->next;
-		}
-		current->next = new;
-		lst->size++;
-	}
-}
-
-Node* ListPop(list* lst)
-{
-	Node* current = lst->head;
-
-	if (current == NULL)
-	{
-		printf("List is empty");
-	}
-	else
-	{
-		lst->head = current->next;
-		lst->size--;
-		return current;
-	}
-}
-
-void NodePrint(Node* n)
-{
-	if (n == NULL)
-	{
-		printf("[]");
-	}
-	else
-	{
-		printf("[%i]", n->dat);
-	}
-}
-
-void ListPrint(list* lst)
-{
-	Node* current = lst->head;
-	if (current == NULL)
-	{
-		printf("List is empty");
-	}
-	else
-	{
-		while (current != NULL)
-		{
-			NodePrint(current);
-			current = current->next;
-			
-		}
-	}
-}
-
-list* ListCopy(list* lst)
-{
-	Node* current1 = lst->head;
-	list* lstCopy = (list*)malloc(sizeof(list));
-	initList(lstCopy);
-
-	if (current1 == NULL)
-	{
-		printf("Nothing to copy");
-	}
-	else
-	{
-		while (current1 != NULL)
-		{
-			ListPush(lstCopy, current1->dat);
-			current1 = current1->next;
-		}
-	}
 	
-	return lstCopy;
+	
 }
 
-boolean SortDetect(list* lst)
+double createTrees(int countOfTree, int countOfUnit)
 {
-	Node* current = lst->head;
-	int counter = 0;
-	int counter2 = 0;
-	if (current == NULL)
+	double counter = 0;
+	while (countOfTree != 0)
 	{
-		printf("%s", "List is empty");
-	}
-	else
-	{
-		while (current->next != NULL)
+		TreeNode* tree = NULL;
+		tree = treeInsert(tree, rand() % 1000 + 1);
+		while (countOfUnit != 0)
 		{
-			if (current->dat < current->next->dat)
-			{
-				counter++;
-			}
-			else if (current->dat > current->next->dat)
-			{
-				counter2++;
-			}
-			current = current->next;
+			treeInsert(tree, rand() % 1000 + 1);
+			countOfUnit--;
 		}
+		if (balanceDetect(tree) == true)
+		{
+			counter += 1;
+		}
+		countOfTree--;
+		countOfUnit = 1000;
 	}
+	return  counter / 50.0 * 100.0;
 
-	if (counter == (lst->size - 1) || counter2 == (lst->size - 1))
-	{
-		return true;
-	}
-	else
+}
+
+boolean binarySearch(TreeNode* root, int data)
+{
+	if (root == NULL)
 	{
 		return false;
 	}
-
-	
+	if (root->key > data)
+	{
+		binarySearch(root->left, data);
+	}
+	else if (root->key < data)
+	{
+		binarySearch(root->right, data);
+	}
+	else if (root->key == data)
+	{
+		return true;
+	}
 }
 
-int main()
+boolean main()
 {
-	list* lst = (list*)malloc(sizeof(list));
-	printf("%i", sequence());
+	TreeNode* tree = NULL;
+
+	tree = treeInsert(tree, 10);
+	treeInsert(tree,8);
+	treeInsert(tree, 19);
+	treeInsert(tree, 5);
+	treeInsert(tree, 9);
+	treeInsert(tree, 16);
+	treeInsert(tree, 21);
+	treeInsert(tree, 22);
+	printTree(tree);
+	printf("\n");
+	printf("%s", balanceDetect(tree)? "True": "False");
+	printf("\n");
+	printf("%i%c", (int)createTrees(50, 1000), '%');
+	printf("\n");
+	printf("%i", binarySearch(tree, 11));
 	
-	printf("\n");
-	initList(lst);
-	ListPush(lst, 30);
-	ListPush(lst, 20);
-	ListPush(lst, 10);
-	ListPrint(lst);
-	printf("\n");
-	ListPrint(ListCopy(lst));
-	printf("\n");
-	ListPrint(lst);
-	printf("\n");
-	printf("%i", SortDetect(lst));
 }
